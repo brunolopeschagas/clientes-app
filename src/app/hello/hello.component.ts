2e9a26bceadcf5bf1007d74c1d293324fa026730
Bruno <bruno.chagas@gmail.com>
2021-10-22 11:44:18 -0300

    campos dinamicos no template html

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'hello',
-    template: `<h1>hello world</h1>`
+    templateUrl: './hello.componente.html'
 })
 export class HelloComponent {
-
+    nome: string;
+    constructor() {
+        this.nome = 'Bruno';
+    }
 }
