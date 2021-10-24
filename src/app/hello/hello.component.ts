1ee070776bc04be6c2bfcc3d944d6d6af144d14c
Bruno <bruno.chagas@gmail.com>
2021-10-24 10:11:43 -0300

    angular arrays - listando clientes

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -7,7 +7,15 @@ import { Component } from '@angular/core';
 })
 export class HelloComponent {
     nome: string;
+    clientes: Cliente[];
     constructor() {
         this.nome = 'Bruno';
+        let fulano = new Cliente('moacir', 12);
+        let ciclano = new Cliente('Cláudio', 44);
+        this.clientes = [fulano, ciclano];
     }
+}
+
+class Cliente{
+    constructor(public nome: string, public idade: number){}
 }
